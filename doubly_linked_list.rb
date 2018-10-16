975dd54696f0855fa46ea54ec14f84014713252d
Ashish Garg <ashish2.kumar@trantorinc.com>
2018-10-16 15:49:27 +0530

    doubly linked list (append, find_tail and print methods added)

--- doubly_linked_list.rb
+++ doubly_linked_list.rb
@@ -6,14 +6,45 @@ class Node
     @next = nil
     @value = value
   end
-
-  def to_s
-    "Node with value: #{@value}"
-  end
 end
 
 class DoublyLinkedList
+  attr_accessor :head
+
   def initialize
-    
+    @head = nil
+  end
+
+  def append(value)
+    node = Node.new(value)
+    if @head
+      old_tail = find_tail
+      old_tail.next = node
+      node.prev = old_tail
+      node.next = nil
+    else
+      @head = node
+    end
+  end
+
+  def find_tail
+    node = @head
+    return node if !node.next
+    while node = node.next
+      return node if !node.next
+    end
+  end
+  
+  def print
+    node = @head
+    if node
+      printable_string = "()[#{node.value}](#{node.next&.value})"
+      while node = node.next
+        printable_string << " -> (#{node.prev&.value})[#{node.value}](#{node.next&.value})"
+      end
+    else
+      puts 'Emply Doubly Linked List'
+    end
+    puts printable_string
   end
 end
