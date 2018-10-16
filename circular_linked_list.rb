0beb4e2aeeb739d3d2bc013ee221af1e8101f770
Ashish Garg <ashish2.kumar@trantorinc.com>
2018-10-16 20:08:32 +0530

    Circular linked list common methods added

--- circular_linked_list.rb
+++ circular_linked_list.rb
@@ -13,5 +13,45 @@ class Node
 end
 
 class CircularLinkedList
+  attr_accessor :head, :current
+
+  def initialize
+    @head = nil
+    @current = nil
+  end
+
+  def append(value)
+    node = Node.new(value)
+    if @head
+      tail = find_tail
+      tail.next = node
+      node.next = @head
+    else
+      @head = node
+      @current = node
+    end
+  end
+
+  def find_tail
+    @current = @head
+    return @current unless @current.next
+    while @current = @current.next
+      return @current if @current.next == @head
+    end
+  end
+
+  def print
+    @current = @head
+    printable_string = "#{@current.value}"
+    while @current = @current.next
+      if @current.next == @head
+        printable_string << " -> #{@current.value}(pointing back to head #{@head.value})"
+        puts printable_string
+        return false
+      else
+        printable_string << " -> #{@current.value}"
+      end
+    end  
+  end
   
 end
