ed267ceda329435283a57b9ffdbdc303ab392391
Ashish Garg <ashish2.kumar@trantorinc.com>
2018-10-23 12:57:41 +0530

    program restructured

--- programs/queue_with_array.rb
+++ programs/queue_with_array.rb
@@ -1,9 +1,3 @@
-=begin
-  Queue(FIFO) implementation with array
-=end
-
-require_relative '../programs/singly_linked_list.rb'
-
 class QueueWithArray
   attr_accessor :front, :rear, :size, :capacity
 
@@ -56,55 +50,4 @@ class QueueWithArray
     printable += ']'
     puts printable
   end
-end
-
-a = QueueWithArray.new(10)
-a.enqueue(1)
-a.print
-a.enqueue(2)
-a.print
-a.enqueue(3)
-a.print
-a.dequeue
-a.print
-
-class QueueWithLinkedList
-  attr_accessor :front, :rear, :length, :linked_list
-  
-  def initialize
-    @length = 0
-    @rear = nil
-    @front = nil
-    @linked_list = LinkedList.new
-  end
-
-  def enqueue(data)
-    node = Node.new(data)
-    @linked_list.append(data)
-    if @linked_list.size == 1
-      @front = node
-    else
-      @rear = node    
-    end
-    @length += 1
-  end
-
-  def dequeue
-    if @linked_list.size == 0
-      raise 'Underflow'
-    else
-      data = @front.value      
-      @linked_list.delete(data)
-      @front = @linked_list.head
-      @length -= 1
-      if @length == 0
-        @rear = nil
-      end      
-      data
-    end
-  end
-
-  def print
-    @linked_list.print
-  end  
 end
---
+++ programs/queue_with_linked_list.rb
@@ -0,0 +1,42 @@
+require_relative '../programs/singly_linked_list.rb'
+
+class QueueWithLinkedList
+    attr_accessor :front, :rear, :length, :linked_list
+    
+    def initialize
+      @length = 0
+      @rear = nil
+      @front = nil
+      @linked_list = LinkedList.new
+    end
+  
+    def enqueue(data)
+      node = Node.new(data)
+      @linked_list.append(data)
+      if @linked_list.size == 1
+        @front = node
+      else
+        @rear = node    
+      end
+      @length += 1
+    end
+  
+    def dequeue
+      if @linked_list.size == 0
+        raise 'Underflow'
+      else
+        data = @front.value      
+        @linked_list.delete(data)
+        @front = @linked_list.head
+        @length -= 1
+        if @length == 0
+          @rear = nil
+        end      
+        data
+      end
+    end
+  
+    def print
+      @linked_list.print
+    end  
+  end
